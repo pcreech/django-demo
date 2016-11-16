9dda5e801ac4afa2136d7222db7baa7f2c127f93
Patrick Creech <pcreech@redhat.com>
2016-11-16 16:05:26 -0500

    Reduce initial wait time
    Reduce initial wait time

--- mysite/addtask/views.py
+++ mysite/addtask/views.py
@@ -15,7 +15,7 @@ def add(request, left=0, right=0):
     req.save()
     result = addtask.delay(req.left, req.right)
     while not result.ready():
-       time.sleep(10)
+       time.sleep(1)
     req.total = result.get(timeout=20)
     req.save()
     output = "{} + {} = {}".format(req.left, req.right, req.total)
